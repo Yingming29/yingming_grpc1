8ba268540caa624e390733f46d2705e66a1d22af
Yingming29 <yingmingw97@gmail.com>
2021-05-28 16:08:49 +0800

    jchannel service. It has the basic functions, disconnect mode, update the server addresses, send() broadcast or unicast message, connect(), disconnect, the change of view, protobuf transmission between nodes(real jchannel), the changed coordinator of view, the state() for the message history.

--- grpc/src/main/java/cn/yingming/grpc1/SimpleChat.java
+++ grpc/src/main/java/cn/yingming/grpc1/SimpleChat.java
@@ -1,32 +1,22 @@
 package cn.yingming.grpc1;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.util.LinkedList;
-import java.util.List;
 
-import com.google.protobuf.InvalidProtocolBufferException;
-import io.grpc.jchannelRpc.ConnectReq;
-import io.grpc.jchannelRpc.Request;
+import java.io.InputStreamReader;
+
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.ObjectMessage;
 import org.jgroups.Receiver;
 import org.jgroups.View;
-import org.jgroups.util.Util;
+
 
 public class SimpleChat implements Receiver{
 	JChannel channel;
 	String user_name = System.getProperty("user.name", "n/a");
-	final List<String> state = new LinkedList<String>();
 	private void start() throws Exception {
 		channel = new JChannel();
 		channel.setReceiver(this).connect("ChatCluster");
-		channel.getState(null, 10000);
 		eventLoop();
 		channel.close();
 	}
@@ -36,46 +26,15 @@ public class SimpleChat implements Receiver{
 	}
 	@Override
 	public void receive(Message msg) {
-		String line = msg.getSrc() + ":" + msg.getObject();
-		System.out.println("Before:" + line);
-		try {
-			/*
-			Request req = Request.parseFrom((byte[]) msg.getPayload());
-			System.out.println("After:" + req);
-			ConnectReq req2 = req.getConnectRequest();
-			System.out.println(req2);
-			 */
-			Object obj =  Utils.unserializeClusterInf((byte[]) msg.getPayload());
-			System.out.println(obj);
-			System.out.println("-------");
-			ClusterMap m2 = (ClusterMap) obj;
-			System.out.println(m2.getCreator());
-			System.out.println(m2.getMap());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		String line = msg.getSrc() + ":" + msg.getPayload();
+		System.out.println("Before decode:" + line);
 
-		synchronized (state) {
-			state.add(line);
-		}
+		String content = new String((byte[]) msg.getPayload());
+		line = msg.getSrc() + ":" + content;
+		System.out.println("After decode:" + line);
+
 	}
-	@Override
-	public void getState(OutputStream output) throws Exception {
-		synchronized (state) {
-			Util.objectToStream(state, new DataOutputStream(output));
-		}
-	}
-	@Override
-	public void setState(InputStream input) throws Exception{
-		List<String> list;
-		list = (List<String>)Util.objectFromStream(new DataInputStream(input));
-		synchronized (state) {
-			state.clear();
-			state.addAll(list);
-		}
-		System.out.println(list.size() + " messages in chat history.");
-		list.forEach(System.out::println);
-	}
+
 	private void eventLoop() {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -87,22 +46,10 @@ public class SimpleChat implements Receiver{
 				if (line.startsWith("quit") || line.startsWith("exit")) {
 					break;
 				}
-				/*
-				ConnectReq crq = ConnectReq.newBuilder()
-						.setSource("source")
-						.setCluster(line)
-						.build();
-				Request req = Request.newBuilder()
-						.setConnectRequest(crq)
-						.build();
-						byte[] b =  req.toByteArray();
-				*/
-				ClusterMap m = new ClusterMap("TestCreator");
-				m.getMap().put("Testuuid", "TestAddress");
-				byte[] b = Utils.serializeClusterInf(m);
+				byte[] b = line.getBytes();
 
-				// destination address is null, send msg to everyone in the cluster.
 				Message msg = new ObjectMessage(null, b);
+				msg.setFlagIfAbsent(Message.TransientFlag.DONT_LOOPBACK);
 				channel.send(msg);
 			} catch (Exception e) {
 				// TODO: handle exception
